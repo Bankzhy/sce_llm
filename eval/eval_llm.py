61c7b7c50c32337123099b8255544f770bb34cb3
zhang.rog <bankzhy@gmail.com>
2026-03-07 14:45:39 +0900

    init

--- eval/eval_llm.py
+++ eval/eval_llm.py
@@ -10,7 +10,7 @@ load_in_4bit = True
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name="unsloth/codellama-7b-bnb-4bit",
-    max_seq_length=max_seq_length,
+    # max_seq_length=max_seq_length,
     dtype=dtype,
     load_in_4bit=load_in_4bit,
     # token = "YOUR_HF_TOKEN", # HF Token for gated models
