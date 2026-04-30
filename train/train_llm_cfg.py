f44ef91a02554e3fc57ecfa511a1c5803c2f0ef7
zhang.rog <bankzhy@gmail.com>
2026-04-30 16:27:26 +0900

    init

--- train/train_llm_cfg.py
+++ train/train_llm_cfg.py
@@ -18,7 +18,8 @@ dtype = None
 load_in_4bit = True
 model, tokenizer = FastLanguageModel.from_pretrained(
     # model_name = "unsloth/codellama-7b-bnb-4bit",
-    model_name= "unsloth/Qwen2.5-Coder-7B-Instruct-bnb-4bit",
+    # model_name= "unsloth/Qwen2.5-Coder-7B-Instruct-bnb-4bit",
+    model_name= "deepseek-ai/deepseek-coder-6.7b-instruct",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
@@ -199,8 +200,12 @@ if __name__ == '__main__':
     # 保存微调模型
     # model.save_pretrained("lora_model_1")
     # tokenizer.save_pretrained("lora_model_1")
-    model.save_pretrained("lora_model_qwen_coder")
-    tokenizer.save_pretrained("lora_model_qwen_coder")
+
+    # model.save_pretrained("lora_model_qwen_coder")
+    # tokenizer.save_pretrained("lora_model_qwen_coder")
+
+    model.save_pretrained("lora_model_deepseek_coder")
+    tokenizer.save_pretrained("lora_model_deepseek_coder")
 
     # 合并模型，保存为16位hf
     # model.save_pretrained_merged("outputs", tokenizer, save_method="merged_16bit", )
